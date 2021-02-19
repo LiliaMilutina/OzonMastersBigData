95d6585056b9c62432ec74063033017a84a9cfb4
Lilia <sergeenkova.lilia@yandex.ru>
2021-02-19 15:13:08 +0300

    Update filter_cond.py

--- projects/1/filter_cond.py
+++ projects/1/filter_cond.py
@@ -6,7 +6,7 @@ def filter_cond(line_dict):
     Returns True if conditions are satisfied
     """
     cond_match = (
-       int(line_dict["num_reviews"]) > 20 and int(line_dict["num_reviews"]) < 40
+       int(line_dict["if1"]) > 20 and int(line_dict["if1"]) < 40
     ) 
     return True if cond_match else False
 
