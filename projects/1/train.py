b5eaf9886a304c5510871294ab92ba61e365b0e2
Lilia <sergeenkova.lilia@yandex.ru>
2021-02-19 11:31:41 +0300

    Update train.py

--- projects/1/train.py
+++ projects/1/train.py
@@ -41,7 +41,7 @@ logging.info(f"TRAIN_PATH {train_path}")
 #fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
 #num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
 
-read_table_opts = dict(sep="  ", names=fields, index_col=False)
+read_table_opts = dict(sep="\t", names=fields, index_col=False)
 df = pd.read_table(train_path, **read_table_opts)
 
 #split train/test
