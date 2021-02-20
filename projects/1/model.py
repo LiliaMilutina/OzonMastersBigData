8560d9f0178597770d72db40159ccee0c70154c1
Lilia <sergeenkova.lilia@yandex.ru>
2021-02-20 10:24:14 +0300

    Update model.py

--- projects/1/model.py
+++ projects/1/model.py
@@ -16,24 +16,31 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 
 # We create the preprocessing pipelines for both numeric and categorical data.
 numeric_features = ["if"+str(i) for i in range(1,14)]
-categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+# categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
 
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
 #    ('scaler', StandardScaler())
 ])
 
-categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))
-])
+# categorical_transformer = Pipeline(steps=[
+#     ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+#     ('onehot', OneHotEncoder(handle_unknown='ignore'))
+# ])
 
-fields = ["id", "label"] + numeric_features + categorical_features
+# fields = ["id", "label"] + numeric_features + categorical_features
+fields = ["id", "label"] + numeric_features
+
+# preprocessor = ColumnTransformer(
+#     transformers=[
+#         ('num', numeric_transformer, numeric_features),
+#         ('cat', categorical_transformer, categorical_features)
+#     ]
+# )
 
 preprocessor = ColumnTransformer(
     transformers=[
-        ('num', numeric_transformer, numeric_features),
-        ('cat', categorical_transformer, categorical_features)
+        ('num', numeric_transformer, numeric_features)
     ]
 )
 
