7900998a16c6acae66982ab199b37c3b3455aae3
Lilia <sergeenkova.lilia@yandex.ru>
2021-02-24 17:02:15 +0300

    Update predict.py

--- projects/1/predict.py
+++ projects/1/predict.py
@@ -7,7 +7,10 @@ import pandas as pd
 
 # sys.path.append('.')
 
-from model import fields_val
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+
+fields_val = ["id"] + numeric_features + categorical_features
 
 #
 # Init the logger
