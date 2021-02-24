b030fcef25fe2ea30742de65beaa0e1f730bfcd6
Lilia <sergeenkova.lilia@yandex.ru>
2021-02-24 14:24:16 +0300

    Update filter.py

--- projects/1/filter.py
+++ projects/1/filter.py
@@ -8,69 +8,69 @@ import logging
 sys.path.append('.')
 from model import fields_val
 
-#
-# Init the logger
-#
-logging.basicConfig(level=logging.DEBUG)
-logging.info("CURRENT_DIR {}".format(os.getcwd()))
-logging.info("SCRIPT CALLED AS {}".format(sys.argv[0]))
-logging.info("ARGS {}".format(sys.argv[1:]))
+# #
+# # Init the logger
+# #
+# logging.basicConfig(level=logging.DEBUG)
+# logging.info("CURRENT_DIR {}".format(os.getcwd()))
+# logging.info("SCRIPT CALLED AS {}".format(sys.argv[0]))
+# logging.info("ARGS {}".format(sys.argv[1:]))
 
-#
-# import the filter
-#
-filter_cond_files = glob('filter_cond*.py')
-logging.info(f"FILTERS {filter_cond_files}")
+# #
+# # import the filter
+# #
+# filter_cond_files = glob('filter_cond*.py')
+# logging.info(f"FILTERS {filter_cond_files}")
 
-if len(filter_cond_files) != 1:
-    logging.critical("Must supply exactly one filter")
-    sys.exit(1)
+# if len(filter_cond_files) != 1:
+#     logging.critical("Must supply exactly one filter")
+#     sys.exit(1)
 
-exec(open(filter_cond_files[0]).read())
+# exec(open(filter_cond_files[0]).read())
 
-#
-# dataset fields
-#
-#fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
-#num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
+# #
+# # dataset fields
+# #
+# #fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
+# #num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
 
-#
-# Optional argument
-# If +field option is given, output the id (always first record) and the given field
-# if -field is given, output all but the given field
-#
+# #
+# # Optional argument
+# # If +field option is given, output the id (always first record) and the given field
+# # if -field is given, output all but the given field
+# #
 
-if len(sys.argv) == 1:
-  #by default print all fields
-  outfields = fields_val
-else:
-  op, field = sys.argv[1][0], sys.argv[1][1:]
-  logging.info(f"OP {op}")
-  logging.info(f"FIELD {field}")
+# if len(sys.argv) == 1:
+#   #by default print all fields
+#   outfields = fields_val
+# else:
+#   op, field = sys.argv[1][0], sys.argv[1][1:]
+#   logging.info(f"OP {op}")
+#   logging.info(f"FIELD {field}")
 
-  if not op in "+-" or not field in fields_val:
-    logging.critical("The optional argument must start with + or - followed by a valid field")
-    sys.exit(1)
-  elif op == '+':
-    outfields = [fields_val[0], field]
-  else:
-    outfields = list(fields_val) # like deepcopy, but on the first level only!
-    outfields.remove(field)
+#   if not op in "+-" or not field in fields_val:
+#     logging.critical("The optional argument must start with + or - followed by a valid field")
+#     sys.exit(1)
+#   elif op == '+':
+#     outfields = [fields_val[0], field]
+#   else:
+#     outfields = list(fields_val) # like deepcopy, but on the first level only!
+#     outfields.remove(field)
 
 
 
-for line in sys.stdin:
-    # skip header
-    if line.startswith(fields_val[0]):
-        continue
+# for line in sys.stdin:
+#     # skip header
+#     if line.startswith(fields_val[0]):
+#         continue
 
-    #unpack into a tuple/dict
-    values = line..rstrip().split('\t')
-    record = dict(zip(fields_val, values)) #Hotel(values)
+#     #unpack into a tuple/dict
+#     values = line.rstrip().split('\t')
+#     record = dict(zip(fields_val, values)) #Hotel(values)
 
-    #apply filter conditions
-    if filter_cond(record):
-        output = "\t".join([record[x] for x in outfields])
-        print(output)
+#     #apply filter conditions
+#     if filter_cond(record):
+#         output = "\t".join([record[x] for x in outfields])
+#         print(output)
 
 
