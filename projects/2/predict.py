edc15fbc0d9cbd0e616e08197dd397020b4d1e52
Lilia <sergeenkova.lilia@yandex.ru>
2021-03-12 17:01:31 +0300

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -32,11 +32,28 @@
 #     out = zip(df.id, y_pred[:, 1])
 #     print("\n".join(["{0}\t{1}".format(*i) for i in out]))
 
-import sys
+items_sold = []  # create global list variable
 
-while True:
-    line = sys.stdin.readline()
-    if not line:
-        break
-    print("123\t0»)
+class Items:  # create class to store and access items added
+    def __init__(self, x):
+    	self.x = x
+
+    def set_x(self, x):
+        self.x = x
+    
+    def get_x(self):
+        return self.x
+
+def print_results():  # print output in Hive
+	result_set = [item.get_x() for item in items_sold];
+	print (result_set)
+
+	# Hive submits each record to stdin
+	# The record/line is stripped of extra characters and submitted
+for line in sys.stdin:
+	line = line.strip()
+	purchased_item = line.split('\t')
+	items_sold.append(Items(purchased_item))
+
+print_results()
 
