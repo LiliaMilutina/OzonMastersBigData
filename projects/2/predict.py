ad1a8cb58d85f68da8f3751f3dc89363a09f7191
Lilia <sergeenkova.lilia@yandex.ru>
2021-03-15 12:55:01 +0300

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -1,12 +1,26 @@
 #!/opt/conda/envs/dsenv/bin/python
 
-import sys
+import sys, os
+from joblib import load
+import pandas as pd
+import numpy as np
 
-while True:
-    line = sys.stdin.readline()
-    if not line:
-        break
-    print("123\t0»)
+from model import model, fields_val
+
+# #load the model
+# model = load("2.joblib")
+
+#read and infere
+read_opts=dict(
+        sep='\t', names=fields_val, index_col=False, header=None,
+        iterator=True, chunksize=100, na_values='\\N'
+)
+
+for df in pd.read_csv(sys.stdin, **read_opts):
+#     y_pred = model.predict_proba(df)
+#     out = zip(df.id, y_pred[:, 1])
+#     print("\n".join(["{0}\t{1}".format(*i) for i in out]))
+    print("1\t2")
 
 
 
