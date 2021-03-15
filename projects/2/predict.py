766b62380e652eafc95baf6ac1af123764cd6f13
Lilia <sergeenkova.lilia@yandex.ru>
2021-03-15 08:33:36 +0300

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -7,22 +7,12 @@ from joblib import load
 import pandas as pd
 import numpy as np
 
-# sys.path.append('.')
-
 from model import model, fields_val
-
-
-# # Init the logger
-# #
-# logging.basicConfig(level=logging.DEBUG)
-# logging.info("CURRENT_DIR {}".format(os.getcwd()))
-# logging.info("SCRIPT CALLED AS {}".format(sys.argv[0]))
-# logging.info("ARGS {}".format(sys.argv[1:]))
 
 #load the model
 # model = load("2.joblib")
 
-read and infere
+# read and infere
 read_opts=dict(
         sep='\t', names=fields_val, index_col=False, header=None,
         iterator=True, chunksize=100, na_values = '\\N'
