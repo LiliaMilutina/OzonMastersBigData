f2e67f0832f61b622898532dd4c69e1d7a5c9022
Lilia <sergeenkova.lilia@yandex.ru>
2021-04-15 19:14:57 +0300

    Update train.py

--- projects/4/train.py
+++ projects/4/train.py
@@ -2,6 +2,15 @@ import os
 import sys
 import random
 
+SPARK_HOME = "/usr/hdp/current/spark2-client"
+PYSPARK_PYTHON = "/opt/conda/envs/dsenv/bin/python"
+os.environ["PYSPARK_PYTHON"]= PYSPARK_PYTHON
+os.environ["SPARK_HOME"] = SPARK_HOME
+
+PYSPARK_HOME = os.path.join(SPARK_HOME, "python/lib")
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "py4j-0.10.7-src.zip"))
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "pyspark.zip"))
+
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
