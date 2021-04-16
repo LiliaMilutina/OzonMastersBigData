e6909b97754a9ac444893b6c2fa3bc48840e8db7
Lilia <sergeenkova.lilia@yandex.ru>
2021-04-16 11:12:15 +0300

    Update predict.py

--- projects/4/predict.py
+++ projects/4/predict.py
@@ -6,11 +6,9 @@ spark.sparkContext.setLogLevel('WARN')
 from pyspark.ml import Pipeline, PipelineModel
 from pyspark.sql import functions as f
 
-model_path = sys.argv[1]
-data_test_path = sys.argv[2]
-result_path = sys.argv[3]
-
-# data_test_path = "/datasets/amazon/all_reviews_5_core_test_features.json"
+model_path = sys.argv[0]
+data_test_path = sys.argv[1]
+result_path = sys.argv[2]
 
 pipeline_model = PipelineModel.load(model_path)
 
