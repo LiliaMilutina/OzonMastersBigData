52f4ed1c904de8a7ee715433a253c54c696101d5
Lilia <sergeenkova.lilia@yandex.ru>
2021-05-06 14:30:51 +0300

    Update train.py

--- projects/5mla/train.py
+++ projects/5mla/train.py
@@ -1,7 +1,6 @@
 #!/opt/conda/envs/dsenv/bin/python
 
 import os, sys
-import logging
 
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -69,8 +68,6 @@ model.fit(X_train, y_train)
 y_pred = model.predict_proba(X_test)
 
 model_score = log_loss(y_test, y_pred[:, 1])
-
-logging.info(f"model score: {model_score:.3f}")
 
 # save the model
 dump(model, "{}.joblib".format(proj_id))
