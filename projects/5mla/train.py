5404ead0cac1f7ddd63438e4b4a9d89daded5b71
Lilia <sergeenkova.lilia@yandex.ru>
2021-05-06 13:14:59 +0300

    Update train.py

--- projects/5mla/train.py
+++ projects/5mla/train.py
@@ -4,15 +4,51 @@ import os, sys
 import logging
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import log_loss
 from joblib import dump
 import numpy as np
 
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+
+
 #
-# Import model definition
+# Model pipeline
 #
-from model import model, fields
+
+# We create the preprocessing pipelines for both numeric and categorical data.
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+#    ('scaler', StandardScaler())
+])
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+fields = ["id", "label"] + numeric_features + categorical_features
+fields_val = ["id"] + numeric_features + categorical_features
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)
+    ]
+)
+
+# Now we have a full prediction pipeline.
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('logregression', LogisticRegression())
+])
 
 #
 # Logging initialization
